50fe7c31fab35956ba905d6259f2648c125c6c8f
zzzzisu <93628749+zzzzisu@users.noreply.github.com>
2022-01-09 22:41:25 +0900

    Delete process directory

--- process/script/process.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $("button").click(function(){
-        var index = $("button").index(this);
-        $("button").removeClass("on");
-        $(".detail").find("img").removeClass("on");
-        $(this).addClass("on");
-        $(".detail").find("img").eq(index).addClass("on");
-    });
-});
