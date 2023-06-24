b82b94c1b5bfd8060a56019a3343d6fc58e1291a
zzzzisu <93628749+zzzzisu@users.noreply.github.com>
2023-06-24 16:45:21 +0900

    Update sub-script.js

--- benz/script/sub-script.js
+++ benz/script/sub-script.js
@@ -1,94 +1,92 @@
 $(function(){
-    $(".nav").hover(
-        function(){
-            if($(".sub").is(":animated")) return;
-            $(".sub").slideDown();},
-            function(){
-            if($(".sub").is(":animated")) return;
-            $(".sub").slideUp();}
+    // nav
+    let nav = $('#nav');
+    let navHeight = $('#nav').outerHeight();
+    nav.css('height',`${navHeight}px`).addClass('hide');
+
+    nav.hover(
+        function(){nav.removeClass('hide')},
+        function(){nav.addClass('hide')}
     );
 
+    var winHeight = $(window).outerHeight();
+    var scrollTop = '';
 
-    $(window).scroll(function(){
-        if($(this).scrollTop() >= 30){
-            $(".nav").css({position:"fixed", top:0})
-        }else{
-            $(".nav").removeAttr("style");
-        }
-    });
-
-    $(".safety_btn > li").on("click",function(){
-        $(".safety_btn > li").removeClass("clicked");
-        $(".tab_bx").removeClass("clicked");
-        let safetyIndex = $(".safety_btn > li").index(this);
-        
-        $(this).addClass("clicked");
-        $(".tab_bx").eq(safetyIndex).addClass("clicked");
-    });
-
-    $(".bx_support > ul > li").on("click",function(){
-        let supportIndex = $(".bx_support > ul > li").index(this);
-        $(".bx_support > ul > li").removeClass("clicked");
-        $(".bx_support > .exp_bx").removeClass("clicked");
-        $(this).addClass("clicked");
-        $(".bx_support > .exp_bx").eq(supportIndex).addClass("clicked");
-    });
-
-    $(".bx_connect > ul > li").on("click",function(){
-        let connectIndex = $(".bx_connect > ul > li").index(this);
-        $(".bx_connect > ul > li").removeClass("clicked");
-        $(".bx_connect > .exp_bx").removeClass("clicked");
-        $(this).addClass("clicked");
-        $(".bx_connect > .exp_bx").eq(connectIndex).addClass("clicked");
-    });
-
-    function parallax(st, th) {
-        
-        if (st >= (th.offset().top - $(window).outerHeight() * 0.8)) {
-            th.addClass("move");
-        } else { th.removeClass("move") };
+    // parallax
+    function parallax_img(a){
+        if(a.offset().top <= scrollTop + (winHeight * 0.7)){
+            a.addClass('move');
+        }else{a.removeClass('move')}
     }
 
-    function img(st, th) {
-        if (st >= th.offset().top - $(window).outerHeight() * 1.1) {
-                th.addClass("move");
-            } else { th.removeClass("move") };
+    function parallax_txt(a){
+        if(a.offset().top <= scrollTop + (winHeight * 0.9)){
+            a.addClass('move');
+        }else{a.removeClass('move')}
     }
 
+    // scroll
     $(window).scroll(function(){
-        var st = $(this).scrollTop();
-        parallax(st,$(".exterior > img"));
-        parallax(st,$(".exterior > div"));
-        parallax(st,$(".exterior > .exp"));
-        parallax(st,$(".interior_bx1> div"));
-        parallax(st,$(".interior_bx1> div > h3"));
-        parallax(st,$(".interior_bx1> div > h4"));
-        parallax(st,$(".interior_bx1> img"));
-        parallax(st,$(".interior_bx2> div"));
-        parallax(st,$(".interior_bx2> div > h3"));
-        parallax(st,$(".interior_bx2> div > h4"));
-        parallax(st,$(".interior_bx2> img"));
-        parallax(st,$(".light > div"));
-        parallax(st,$(".seat > div"));
-        parallax(st,$(".sound > div"));
-        parallax(st,$(".screen > div"));
-        parallax(st,$(".sunroof > div"));
-        parallax(st,$(".driving"));
-        img(st,$(".specifications > ul > li:nth-child(1)"));
-        img(st,$(".specifications > ul > li:nth-child(2)"));
-        img(st,$(".specifications > ul > li:nth-child(3)"));
-        img(st,$(".specifications > ul > li:nth-child(4)"));
-        img(st,$(".specifications > ul > li:nth-child(5)"));
-        img(st,$(".specifications > ul > li:nth-child(6)"));
+        scrollTop = $(this).scrollTop();
 
-        if(st >= $(".safety").offset().top){
-            let i = 0;
-            timerId = setInterval(function(){
-                $(".mark").eq(i).animate({opacity:1});
-                $(".circle").eq(i).animate({opacity:1});
-                i++;
-                if(i>$(".circle").length){clearInterval(timerId);};
-            },300);
-        }
+        // nav fix
+        if(scrollTop >= 30){nav.addClass('fixed')}
+        else{nav.removeClass('fixed')}
+        
+        // parallax
+        parallax_img($('#exterior > img'));
+        parallax_img($('#exterior > div'));
+        parallax_txt($('#exterior > .exp'));
+
+        parallax_img($('.interior_bx1 > div'));
+        parallax_img($('.interior_bx1 > img'));
+
+        parallax_img($('.interior_bx2 > div'));
+        parallax_img($('.interior_bx2 > img'));
+
+        parallax_img($('.light'));
+        parallax_img($('.seat'));
+        parallax_img($('.sound'));
+        parallax_img($('.screen'));
+        parallax_img($('.sunroof'));
+
+        parallax_img($('#safety_img'));
+    
+        parallax_txt($('#specifications > ul > li:nth-child(1)'));
+        parallax_txt($('#specifications > ul > li:nth-child(2)'));
+        parallax_txt($('#specifications > ul > li:nth-child(3)'));
+        parallax_txt($('#specifications > ul > li:nth-child(4)'));
+        parallax_txt($('#specifications > ul > li:nth-child(5)'));
+        parallax_txt($('#specifications > ul > li:nth-child(6)'));
     });
+
+    const safetybtn = $('#safety_bx .btn_bx li');
+    const safetybx = $('#safety_bx .tab_bx');
+
+    function tab_bx(a,b,c){
+        a.removeClass('clicked').eq(c).addClass('clicked');
+        b.removeClass('clicked').eq(c).addClass('clicked');
+    }
+
+    safetybtn.click(function(){
+        var safetyIndex = safetybtn.index(this);
+        tab_bx(safetybtn,safetybx,safetyIndex);
+    });
+
+    const supportbtn = $('.support_btn li');
+    const supportbx = $('.bx_support .exp_bx');
+
+    supportbtn.click(function(){
+        var supportIndex = supportbtn.index(this);
+        tab_bx(supportbtn,supportbx,supportIndex);
+    });
+
+    const connectbtn = $('.connect_btn li');
+    const connectbx = $('.bx_connect .exp_bx');
+
+    connectbtn.click(function(){
+        var connectIndex = connectbtn.index(this);
+        tab_bx(connectbtn,connectbx,connectIndex);
+    });
+
 });
